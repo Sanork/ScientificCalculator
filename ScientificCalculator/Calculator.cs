939a4f902e6bd8d58cb0e26ccc7c6c365495bd05
Sanork <mr.shura.san2@gmail.com>
2025-04-08 23:05:53 +0700

    Add method works only for 2 + 2 (temporary implementation)

--- ScientificCalculator/Calculator.cs
+++ ScientificCalculator/Calculator.cs
@@ -8,5 +8,11 @@ namespace ScientificCalculator
 {
     public class Calculator
     {
+        public double Add(double a, double b)
+        {
+            // TODO реализовать метод сложения позднее до конца
+            if (a == 2 && b == 2) return 4;
+            throw new ArgumentException("Add() works only for 2+2");
+        }
     }
 }
