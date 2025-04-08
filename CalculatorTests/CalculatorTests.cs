e165b297c3cf04366d2807f2655325fd99fa3bcb
Sanork <mr.shura.san2@gmail.com>
2025-04-08 22:29:21 +0700

    Add failing test to check existence of Calculator class

---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,12 @@
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+         [TestMethod]
+         public void CalculatorClass_Exists()
+         {
+            var calc = new ScientificCalculator.Calculator();
+         }
+    }
+}
