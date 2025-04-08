36e3291a4c1340d6d66d69d2bf85400bc47274e8
Sanork <mr.shura.san2@gmail.com>
2025-04-08 22:49:48 +0700

    Failing test for Add method

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -1,12 +1,21 @@
+using ScientificCalculator;
+
 namespace CalculatorTests
 {
     [TestClass]
     public class CalculatorTests
     {
-         [TestMethod]
-         public void CalculatorClass_Exists()
-         {
+        [TestMethod]
+        public void CalculatorClass_Exists()
+        {
             var calc = new ScientificCalculator.Calculator();
-         }
+        }
+
+        [TestMethod]
+        public void CalcAdd2And2Test()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(4, calculator.Add(2, 2));
+        }
     }
 }
