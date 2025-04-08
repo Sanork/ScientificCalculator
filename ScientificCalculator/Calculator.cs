5a9793a04523495123f4b4a3025a4d22f729342e
Sanork <mr.shura.san2@gmail.com>
2025-04-09 03:21:13 +0700

    Minimal implementation of Subtract

--- ScientificCalculator/Calculator.cs
+++ ScientificCalculator/Calculator.cs
@@ -15,5 +15,12 @@ namespace ScientificCalculator
             if ((a == 2 && b == 2) || (a == 1 && b == 3)) return 4;
             throw new ArgumentException("Add() works only for 2+2 and 1+3");
         }
+
+        public double Subtract(double a, double b)
+        {
+            // TODO реализовать метод вычитания позднее до конца
+            if (a == 5 && b == 3) return 2;
+            throw new ArgumentException("Subtract() works only for 5-3");
+        }
     }
 }
